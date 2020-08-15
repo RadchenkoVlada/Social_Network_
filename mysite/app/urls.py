80655d241e1d7a43a65c363afac347d105490bcb
RadchenkoVlada <ladon95@gmail.com>
2020-08-15 17:35:10 +0300

    Project-mysite was created and inside it application-app was created. Classes User and Post were created in models.py

---
+++ mysite/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostView
+
+
+app_name = "app"
+
+# app_name will help us do a reverse look-up latter.
+urlpatterns = [
+    path('app/', PostView.as_view()),
+]
---
+++ mysite/app/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Post
+
+class PostView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        return Response({"posts": posts})
